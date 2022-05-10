b3e2c5863f1647e53d93450f7d98bc4b6e006ca3
minakamel <mina.kamel@mimi.io>
2022-05-10 11:37:29 +0200

    Removed count limit on posts api

--- app/src/main/java/com/minafkamel/postroll/data/posts/PostApi.kt
+++ app/src/main/java/com/minafkamel/postroll/data/posts/PostApi.kt
@@ -2,36 +2,17 @@ package com.minafkamel.postroll.data.posts
 
 import com.apollographql.apollo3.ApolloClient
 import com.apollographql.apollo3.api.ApolloResponse
-import com.apollographql.apollo3.api.Optional
 import com.minafkamel.postroll.data.models.GetAllPostsQuery
-import com.minafkamel.postroll.data.models.GetAllUsersQuery
-import com.minafkamel.postroll.data.models.GetPostQuery
-import com.minafkamel.postroll.data.models.type.PageQueryOptions
-import com.minafkamel.postroll.data.models.type.PaginateOptions
+import com.minafkamel.postroll.data.models.GetPostDetailsQuery
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.retry
 
 class PostApi(private val apolloClient: ApolloClient) {
 
     fun getAllPosts(): Flow<ApolloResponse<GetAllPostsQuery.Data>> {
-        return apolloClient.query(
-            GetAllPostsQuery(
-                Optional.presentIfNotNull(
-                    PageQueryOptions(
-                        paginate = Optional.presentIfNotNull(
-                            PaginateOptions(
-                                Optional.presentIfNotNull(
-                                    1
-                                ), Optional.presentIfNotNull(5)
-                            )
-                        )
-                    )
-                )
-            )
-        ).toFlow()
+        return apolloClient.query(GetAllPostsQuery()).toFlow()
     }
 
-    fun getPost(id: String): Flow<ApolloResponse<GetPostQuery.Data>> {
-        return apolloClient.query(GetPostQuery(id)).toFlow()
+    fun getPostDetails(id: String): Flow<ApolloResponse<GetPostDetailsQuery.Data>> {
+        return apolloClient.query(GetPostDetailsQuery(id)).toFlow()
     }
 }
