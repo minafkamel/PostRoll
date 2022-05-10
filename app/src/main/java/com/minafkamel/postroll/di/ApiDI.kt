620aa4b2fa39a5f2ea89b269575ba79799b71b5f
minakamel <mina.kamel@mimi.io>
2022-05-10 02:38:50 +0200

    Updated API and Network Di

--- app/src/main/java/com/minafkamel/postroll/di/ApiDI.kt
+++ app/src/main/java/com/minafkamel/postroll/di/ApiDI.kt
@@ -1,14 +1,9 @@
 package com.minafkamel.postroll.di
 
-import com.apollographql.apollo3.ApolloClient
-import com.minafkamel.postroll.data.models.PostApi
+import com.minafkamel.postroll.data.PostApi
 import org.koin.dsl.module
 
 val apiModule = module {
 
-    fun providesApi(apolloClient: ApolloClient): PostApi {
-        return PostApi(apolloClient)
-    }
-
-    single { providesApi(get()) }
+    single { PostApi(get()) }
 }
--- app/src/main/java/com/minafkamel/postroll/di/NetworkDI.kt
+++ app/src/main/java/com/minafkamel/postroll/di/NetworkDI.kt
@@ -4,21 +4,33 @@ import android.os.Looper
 import com.apollographql.apollo3.ApolloClient
 import com.apollographql.apollo3.network.okHttpClient
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
 
 val networkModule = module {
 
-    fun providesApolloClient(): ApolloClient {
+    fun providesApolloClient(loggingInterceptor: HttpLoggingInterceptor): ApolloClient {
         check(Looper.myLooper() == Looper.getMainLooper()) {
             "Only the main thread can get the apolloClient instance"
         }
-        val okHttpClient = OkHttpClient.Builder().build()
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
         return ApolloClient.Builder()
             .serverUrl(BASE_URL)
             .okHttpClient(okHttpClient)
             .build()
     }
-    single { providesApolloClient() }
+
+    fun providesLoggingInterceptor(): HttpLoggingInterceptor {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        return interceptor
+    }
+
+    single { providesApolloClient(get()) }
+    single { providesLoggingInterceptor() }
+
 }
 
 const val BASE_URL = "https://graphqlzero.almansi.me/api"
