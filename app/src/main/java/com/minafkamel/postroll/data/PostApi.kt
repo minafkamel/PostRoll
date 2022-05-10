c3b55f56529b229816fbd463417deeee78fa636a
minakamel <mina.kamel@mimi.io>
2022-05-10 02:37:57 +0200

    Updated PostAPI

--- app/src/main/java/com/minafkamel/postroll/data/PostApi.kt
+++ app/src/main/java/com/minafkamel/postroll/data/PostApi.kt
@@ -1,15 +1,36 @@
-package com.minafkamel.postroll.data.models
+package com.minafkamel.postroll.data
 
 import com.apollographql.apollo3.ApolloClient
 import com.apollographql.apollo3.api.ApolloResponse
+import com.apollographql.apollo3.api.Optional
+import com.minafkamel.postroll.data.models.GetAllPostsQuery
+import com.minafkamel.postroll.data.models.GetPostQuery
+import com.minafkamel.postroll.data.models.type.PageQueryOptions
+import com.minafkamel.postroll.data.models.type.PaginateOptions
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.retry
 
 class PostApi(private val apolloClient: ApolloClient) {
 
-    suspend fun getAllPosts(): ApolloResponse<GetAllPostsQuery.Data> {
-        return apolloClient.query(GetAllPostsQuery()).execute()
+    fun getAllPosts(): Flow<ApolloResponse<GetAllPostsQuery.Data>> {
+        return apolloClient.query(
+            GetAllPostsQuery(
+                Optional.presentIfNotNull(
+                    PageQueryOptions(
+                        paginate = Optional.presentIfNotNull(
+                            PaginateOptions(
+                                Optional.presentIfNotNull(
+                                    1
+                                ), Optional.presentIfNotNull(5)
+                            )
+                        )
+                    )
+                )
+            )
+        ).toFlow()
     }
 
-    suspend fun getPost(id: String): ApolloResponse<GetPostQuery.Data> {
-        return apolloClient.query(GetPostQuery(id)).execute()
+    fun getPost(id: String): Flow<ApolloResponse<GetPostQuery.Data>> {
+        return apolloClient.query(GetPostQuery(id)).toFlow()
     }
 }
