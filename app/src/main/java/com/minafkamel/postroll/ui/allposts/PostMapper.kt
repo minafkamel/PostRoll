c2293a2ebb8727a68fa3ea525bbcbc7f1468901f
minakamel <mina.kamel@mimi.io>
2022-05-10 11:25:43 +0200

    Changed PostMapper to take

--- app/src/main/java/com/minafkamel/postroll/ui/allposts/PostMapper.kt
+++ app/src/main/java/com/minafkamel/postroll/ui/allposts/PostMapper.kt
@@ -2,7 +2,12 @@ package com.minafkamel.postroll.ui.allposts
 
 class PostMapper {
 
-    fun map(title: String, body: String) = PostViewEntity(title, """${body.substring(0, 120)}...""")
+    fun map(title: String, body: String) =
+        PostViewEntity(title, """${body.take(BODY_MAX_LENGTH)}...""")
 
     class PostViewEntity(val title: String, val body: String)
+
+    companion object {
+        const val BODY_MAX_LENGTH = 120
+    }
 }
