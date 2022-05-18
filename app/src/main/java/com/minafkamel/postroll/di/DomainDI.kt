da1ad9d7641a7b2fbaa3c0b339f70e3e880622c4
minakamel <mina.kamel@mimi.io>
2022-05-18 17:18:32 +0200

    Added domain module & moved domain files to it

--- app/src/main/java/com/minafkamel/postroll/di/DomainDI.kt
+++ app/src/main/java/com/minafkamel/postroll/di/DomainDI.kt
@@ -1,10 +1,8 @@
 package com.minafkamel.postroll.di
 
-import com.minafkamel.postroll.domain.GetPostDetails
-import com.minafkamel.postroll.domain.GetPosts
 import org.koin.dsl.module
 
 val domainModule = module {
-    factory { GetPosts(get()) }
-    factory { GetPostDetails(get()) }
+    factory { com.minafkamel.postroll.domain.GetPosts(get()) }
+    factory { com.minafkamel.postroll.domain.GetPostDetails(get()) }
 }
